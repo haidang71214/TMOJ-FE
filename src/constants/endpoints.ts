c3d59dc7dc2b535159a38b2144ab7c6ed0731852
haidang71214 <haidang71214@gmail.com>
2026-03-04 23:44:21 +0700

    update middleware

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -13,8 +13,21 @@ const userProfileEndpoint = {
   GET_PROFILE: `${API_PREFIX}/User/me`,
   UPDATE_INFOMATION: `${API_PREFIX}/users/profile`
 }
+const AdminUserEndPoint = {
+  GET_LIST_USER : `${API_PREFIX}/User/list-all`,
+  CREATE_USER : `${API_PREFIX}/User`,
+  GET_DETAIL_USER: `${API_PREFIX}/User/{id}`,
+  DELETE_USER:`${API_PREFIX}/User/Pid}`,
+  LOCK_USER_PUT:`${API_PREFIX}/Auth/users/{id}/lock`,
+   UNLOCK_USER_PUT:`${API_PREFIX}/Auth/users/{id}/unlock`,
+  GET_USER_UNLOCK:`${API_PREFIX}/Auth/users/unlock`,
+    GET_USER_LOCK:`${API_PREFIX}/Auth/users/lock`,
+    POST_ASSIGN_ROLE:`${API_PREFIX}/Auth/users/{id}/assign-role`
+}
+
 export {
   authEndpoint,
-  userProfileEndpoint
+  userProfileEndpoint,
+  AdminUserEndPoint
 };
 
---
+++ src/store/queries/user.ts
@@ -0,0 +1,21 @@
+import { AdminUserEndPoint } from "@/constants/endpoints";
+import { baseApi } from "../base";
+import { Users } from "@/types";
+export const authApi = baseApi.injectEndpoints({
+  overrideExisting: true,
+  endpoints: (builder) => ({
+     getUserList: builder.query<Users[], void>({
+      query: () => ({
+        url: AdminUserEndPoint.GET_LIST_USER,
+        method: "GET",
+      }),
+      providesTags: ["User"], 
+    }),
+
+  }),
+});
+
+
+export const {
+  useGetUserListQuery,
+} = authApi;

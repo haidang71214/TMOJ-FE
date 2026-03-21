86ae3898c907836085a89c5c72c364d0179e3b75
haidang71214 <haidang71214@gmail.com>
2026-03-21 07:41:18 +0700

    update endpoints

--- src/utils/baseQueryWithReauth.ts
+++ src/utils/baseQueryWithReauth.ts
@@ -5,11 +5,11 @@ import {
   FetchBaseQueryError,
 } from "@reduxjs/toolkit/query/react";
 import webStorageClient from "@/utils/webStorageClient";
-import { BASE_URLS } from "@/constants";
+import { BASE_URL, BASE_URLS } from "@/constants";
 
 
 export const rawBaseQuery = fetchBaseQuery({
-  baseUrl: BASE_URLS,
+  baseUrl: BASE_URL,
 
   prepareHeaders: (headers, { endpoint }) => {
     const token = webStorageClient.getToken();
